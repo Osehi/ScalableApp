b3a9cfe758e4ef0f1b52670cb0852d07b0e1ac1b
Osehi <osehiaseehilen@gmail.com>
2025-04-17 09:28:32 +0100

    added a snackbar

--- app/src/main/java/com/polishnet/displayusername/screen/DisplayUsernameScreen.kt
+++ app/src/main/java/com/polishnet/displayusername/screen/DisplayUsernameScreen.kt
@@ -5,14 +5,19 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SnackbarHost
+import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -23,6 +28,7 @@ import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.polishnet.displayusername.presentation.DisplayUserNameViewModel
+import kotlinx.coroutines.launch
 
 @Composable
 fun DisplayUsernameScreen(
@@ -33,62 +39,78 @@ fun DisplayUsernameScreen(
     }
     val displayUsernameUIState by displayUserNameViewModel.displayUsername.collectAsStateWithLifecycle()
     var text by remember { mutableStateOf("") }
+    val scope = rememberCoroutineScope()
+    val snackbarHostState = remember{SnackbarHostState()}
 
-    Column(
-        modifier = Modifier,
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Center
-    ) {
-        Log.e("storage", "saved value is ${displayUsernameUIState.username?:""}")
+   Scaffold(
+       snackbarHost = {
+           SnackbarHost(hostState = snackbarHostState)
+       }
 
-        Text(
-            text = displayUsernameUIState.username?:"",
-            style = TextStyle(
-                fontSize = 22.sp
-            ),
-            fontWeight = FontWeight.Bold
-        )
-        Spacer(
-            modifier = Modifier.height(16.dp)
-        )
-        OutlinedTextField(
-            value = text ,
-            onValueChange = {text = it},
-            label = { Text("Enter username") }
-        )
-        Spacer(
-            modifier = Modifier.height(16.dp)
-        )
-        Button(
-            onClick = {
-                displayUserNameViewModel.saveUsername(text)
-                Log.e("storage", "saved value at ave button is ${text}")
-            }
-        ) {
-            Text(
-                text = "Save Username",
-                style = TextStyle(
-                    fontSize = 22.sp
-                ),
-                fontWeight = FontWeight.Bold
-            )
-        }
-        Spacer(
-            modifier = Modifier.height(16.dp)
-        )
-        Button(
-            onClick = {
-                displayUserNameViewModel.displayUsername()
-            }
-        ) {
-            Text(
-                text = "Display Username",
-                style = TextStyle(
-                    fontSize = 22.sp
-                ),
-                fontWeight = FontWeight.Bold
-            )
-        }
+   ) { paddingValues ->
+       Column(
+           modifier = Modifier.padding(paddingValues = paddingValues),
+           horizontalAlignment = Alignment.CenterHorizontally,
+           verticalArrangement = Arrangement.Center
+       ) {
+           Log.e("storage", "saved value is ${displayUsernameUIState.username?:""}")
 
-    }
+           Text(
+               text = displayUsernameUIState.username?:"",
+               style = TextStyle(
+                   fontSize = 22.sp
+               ),
+               fontWeight = FontWeight.Bold
+           )
+           Spacer(
+               modifier = Modifier.height(16.dp)
+           )
+           OutlinedTextField(
+               value = text ,
+               onValueChange = {text = it},
+               label = { Text("Enter username") }
+           )
+           Spacer(
+               modifier = Modifier.height(16.dp)
+           )
+           Button(
+               onClick = {
+                   displayUserNameViewModel.saveUsername(text)
+                   scope.launch {
+                       snackbarHostState.showSnackbar("Usename is saved")
+                   }
+                   Log.e("storage", "saved value at ave button is ${text}")
+               }
+           ) {
+               Text(
+                   text = "Save Username",
+                   style = TextStyle(
+                       fontSize = 22.sp
+                   ),
+                   fontWeight = FontWeight.Bold
+               )
+           }
+           Spacer(
+               modifier = Modifier.height(16.dp)
+           )
+           Button(
+               onClick = {
+                   displayUserNameViewModel.displayUsername()
+               }
+           ) {
+               Text(
+                   text = "Display Username",
+                   style = TextStyle(
+                       fontSize = 22.sp
+                   ),
+                   fontWeight = FontWeight.Bold
+               )
+           }
+
+       }
+
+
+
+   }
+
 }
