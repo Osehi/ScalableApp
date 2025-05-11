2b4052d068644249ced32ba0766e21761ab41fd8
Osehi <osehiaseehilen@gmail.com>
2025-05-11 08:24:37 +0100

    add hiltTestRunner

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -84,4 +84,8 @@ dependencies {
     implementation("androidx.navigation:navigation-compose:2.7.6")
     implementation("androidx.hilt:hilt-navigation-compose:1.2.0")
 
+    // Hilt testing
+    androidTestImplementation("com.google.dagger:hilt-android-testing:2.55")
+    kaptAndroidTest("com.google.dagger:hilt-compiler:2.55")
+
 }
--- app/src/androidTest/java/com/polishnet/displayusername/screen/DisplayUsernameScreenKtTest.kt
+++ app/src/androidTest/java/com/polishnet/displayusername/screen/DisplayUsernameScreenKtTest.kt
@@ -1,21 +1,38 @@
 package com.polishnet.displayusername.screen
 
+import androidx.compose.ui.test.MainTestClock
+import androidx.compose.ui.test.junit4.createAndroidComposeRule
 import androidx.compose.ui.test.junit4.createComposeRule
+import com.polishnet.displayusername.MainActivity
+import com.polishnet.displayusername.di.NetworkModule
+import dagger.hilt.android.testing.HiltAndroidRule
+import dagger.hilt.android.testing.HiltAndroidTest
+import dagger.hilt.android.testing.UninstallModules
 import org.junit.Assert.*
+import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 
+@HiltAndroidTest
 class DisplayUsernameScreenKtTest {
-    @get: Rule  val composeTestRule = createComposeRule()
 
-    // Launch DisplayUSernameUI
+    @get:Rule(order = 0)
+    val hiltRule = HiltAndroidRule(this)
+
+    @get: Rule (order = 1)
+    val composeTestRule = createAndroidComposeRule<MainActivity>()
+
+    @Before
+    fun setup() {
+        hiltRule.inject()
+    }
+
+    // Launch DisplayUsernameUI
     @Test
     fun displayUsernameUI() {
         // start the App
         composeTestRule.setContent {
-            DisplayUsernameScreen(
-
-            )
+            DisplayUsernameScreen()
         }
     }
 }
